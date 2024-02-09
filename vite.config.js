6d0eaa13781021333ca591b3ccfc8b15ee38b041
SachinthaWeerasinghe <153478703+SachinthaWeerasinghe@users.noreply.github.com>
2024-02-09 08:57:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  server: {
       proxy: {
         '/user': 'https://ckd-risk-prediction-backend-python.azurewebsites.net',
       },
-    },
+  },
+  base: './',
 })
